bc6a6e2dc3e39b2db8a28b75b0c21aa58015c38f
keyyuwup <131652456+keyyuwup@users.noreply.github.com>
2025-10-28 02:06:12 +0200

    Add files via upload

--- assets/scripts/scripts.js
+++ assets/scripts/scripts.js
@@ -9,34 +9,22 @@ burgerMenu.addEventListener('click', () => {
     headerNav.classList.toggle('header-nav__open')
 })
 
-// Відкриття модального вікна
 function openModal() {
     document.getElementById('modal').style.display = 'flex';
 }
 
-// Закриття модального вікна
 function closeModal() {
     document.getElementById('modal').style.display = 'none';
 }
 
-// Закриття при кліку поза модальним вікном (miss click)
 document.getElementById('modal').addEventListener('click', function(event) {
     if (event.target === this) {
         closeModal();
     }
 });
 
-// Обробка відправлення форми
 document.getElementById('modalForm').addEventListener('submit', function(event) {
     event.preventDefault();
     alert('Форма надіслана!');
     closeModal();
 });
-
-function openModal() {
-    document.getElementById('modal').style.display = 'block';
-}
-
-function closeModal() {
-    document.getElementById('modal').style.display = 'none';
-}
